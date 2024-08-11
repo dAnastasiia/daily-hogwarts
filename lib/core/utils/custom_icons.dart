7fa69f2d61c9fddbc4d2f2337a5c8e85cedb3714
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-08-11 18:34:28 +0300

    setup dependencies and analyser rules

---
+++ lib/core/utils/custom_icons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData blazon =
+      IconData(0xe95e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wand =
+      IconData(0xf0d0, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData diamond =
+      IconData(0xf219, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData castle =
+      IconData(0xf286, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
