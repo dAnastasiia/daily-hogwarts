ad924a255636fdf6b947307c8af6400eb426440b
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-08-29 22:22:46 +0300

    add auth and start pages

---
+++ lib/features/auth/login_page.dart
@@ -0,0 +1,49 @@
+import 'package:daily_hogwarts/core/utils/methods/getPrettifiedWidgetsList.dart';
+import 'package:daily_hogwarts/core/widgets/custom_filled_button.dart';
+import 'package:daily_hogwarts/core/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Home, sweet home',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 54),
+            ...getPrettifiedWidgetsList(
+              entries: [
+                const CustomTextField(label: 'Username'),
+                const CustomTextField(label: 'Password'),
+              ],
+              spacing: 20,
+            ),
+            const SizedBox(height: 48),
+            CustomFilledButton(
+              onPressed: () {},
+              title: 'Login',
+            ),
+            const SizedBox(height: 12),
+            TextButton(
+              onPressed: () {},
+              child: const Text("Sign up"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/signup_page.dart
@@ -0,0 +1,77 @@
+import 'package:daily_hogwarts/core/utils/methods/getPrettifiedWidgetsList.dart';
+import 'package:daily_hogwarts/core/widgets/custom_filled_button.dart';
+import 'package:daily_hogwarts/core/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Main information',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 12),
+            ...getPrettifiedWidgetsList(
+              entries: [
+                const CustomTextField(
+                  label: 'Username',
+                ),
+                const CustomTextField(
+                  label: 'Email',
+                ),
+                const CustomTextField(
+                  label: 'Password',
+                ),
+              ],
+              spacing: 20,
+            ),
+            const SizedBox(height: 60),
+            const Text(
+              'Sorting Hat Questions',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 12),
+            ...getPrettifiedWidgetsList(
+              entries: [
+                const CustomTextField(
+                  label: 'What is your creature?',
+                ),
+                const CustomTextField(
+                  label: 'What is your best quality?',
+                ),
+                const CustomTextField(
+                  label: 'What is your dream?',
+                ),
+              ],
+              spacing: 20,
+            ),
+            const SizedBox(height: 60),
+            CustomFilledButton(
+              title: 'Let\'s go',
+              onPressed: () {},
+              backgroundColor: Colors.deepPurple,
+              foregroundColor: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
