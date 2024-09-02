2dfc056938da746afd6c14513a86662fd52434d9
Anastasiia Drozd <da4798500@gmail.com>
2024-09-02 20:56:16 +0300

    Add preset for navigaton (#10)

--- lib/core/ui/prettified_field_value.dart
+++ lib/core/ui/prettified_field_value.dart
@@ -1,4 +1,6 @@
+import 'package:daily_hogwarts/core/model/theme_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PrettifiedFieldValue extends StatelessWidget {
   final String title;
@@ -12,20 +14,24 @@ class PrettifiedFieldValue extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeMode themeMode = context.watch<ThemeViewModel>().themeMode;
+
     return RichText(
       text: TextSpan(
         text: title,
-        style: const TextStyle(
+        style: TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.bold,
+          color: themeMode == ThemeMode.light ? Colors.black : Colors.white,
         ),
         children: <TextSpan>[
           TextSpan(
             text: value,
-            style: const TextStyle(
+            style: TextStyle(
               fontSize: 18,
               fontWeight: FontWeight.normal,
               fontStyle: FontStyle.italic,
+              color: themeMode == ThemeMode.light ? Colors.black : Colors.white,
             ),
           ),
         ],
--- lib/features/characters/characters_page.dart
+++ lib/features/characters/characters_page.dart
@@ -1,6 +1,6 @@
 import 'package:daily_hogwarts/core/utils/mock_characters.dart';
 import 'package:daily_hogwarts/features/characters/character_details_page.dart';
-import 'package:daily_hogwarts/features/characters/ui/character_list.dart';
+import 'package:daily_hogwarts/features/characters/ui/character_item.dart';
 import 'package:flutter/material.dart';
 
 class CharactersPage extends StatelessWidget {
@@ -8,16 +8,18 @@ class CharactersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CharacterList(
-      characters: characters,
-      onClick: (character) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => CharacterDetailsPage(
-              character: character,
-            ),
-          ),
+    return ListView.builder(
+      itemCount: characters.length,
+      itemBuilder: (_, index) {
+        final character = characters[index];
+
+        return CharacterItem(
+          character: character,
+          onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => CharacterDetailsPage(character: character),
+              )),
         );
       },
     );
