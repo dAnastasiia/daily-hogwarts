796aeb63fd16e6c9da946c0b098ebe29e1aff423
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-09-07 16:23:33 +0300

    add house page adaptiveness

---
+++ lib/features/house/ui/favourite_spells.dart
@@ -0,0 +1,76 @@
+import 'package:daily_hogwarts/core/model/auth_view_model.dart';
+import 'package:daily_hogwarts/core/ui/prettified_field_value.dart';
+import 'package:daily_hogwarts/core/utils/mock_spells.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class FavouriteSpells extends StatelessWidget {
+  const FavouriteSpells({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: PrettifiedFieldValue(
+              title: "Favourite spells",
+            ),
+          ),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: spells.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisSpacing: 16,
+              childAspectRatio: 4,
+              crossAxisCount: orientation == Orientation.portrait ? 2 : 4,
+              crossAxisSpacing: 16,
+            ),
+            itemBuilder: (_, index) {
+              return _SpellsGridItem(
+                text: spells[index].name,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SpellsGridItem extends StatelessWidget {
+  final String text;
+
+  const _SpellsGridItem({
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<AuthViewModel, Color>(
+      selector: (_, provider) => provider.house.color,
+      builder: (_, color, ___) => Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+        alignment: AlignmentDirectional.center,
+        decoration: ShapeDecoration(
+          shape: StadiumBorder(
+            side: BorderSide(color: color),
+          ),
+        ),
+        child: Text(
+          text,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+    );
+  }
+}
