03827c144225ecc7ea43ff5689534087df831efc
Anastasiia Drozd <da4798500@gmail.com>
2024-09-12 20:38:52 +0300

    Update ui (#17)

--- lib/core/utils/custom_icons.dart
+++ lib/core/utils/custom_icons.dart
@@ -6,12 +6,24 @@ class CustomIcons {
   static const fontFamily = 'CustomIcons';
   static const String? fontPackage = null;
 
-  static const IconData blazon =
-      IconData(0xe95e, fontFamily: fontFamily, fontPackage: fontPackage);
-  static const IconData wand =
-      IconData(0xf0d0, fontFamily: fontFamily, fontPackage: fontPackage);
-  static const IconData diamond =
-      IconData(0xf219, fontFamily: fontFamily, fontPackage: fontPackage);
-  static const IconData castle =
-      IconData(0xf286, fontFamily: fontFamily, fontPackage: fontPackage);
+  static const IconData blazon = IconData(
+    0xe95e,
+    fontFamily: fontFamily,
+    fontPackage: fontPackage,
+  );
+  static const IconData wand = IconData(
+    0xf0d0,
+    fontFamily: fontFamily,
+    fontPackage: fontPackage,
+  );
+  static const IconData diamond = IconData(
+    0xf219,
+    fontFamily: fontFamily,
+    fontPackage: fontPackage,
+  );
+  static const IconData castle = IconData(
+    0xf286,
+    fontFamily: fontFamily,
+    fontPackage: fontPackage,
+  );
 }
