53b59f09101070f2104593c79f2f4e45eb9a5db7
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-10-09 21:07:00 +0300

    add isObscured option to custom text field

--- lib/core/ui/custom_text_field.dart
+++ lib/core/ui/custom_text_field.dart
@@ -2,11 +2,12 @@ import 'package:daily_hogwarts/core/utils/forms/validation.dart';
 import 'package:daily_hogwarts/core/utils/forms/validator.dart';
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final String labelText;
   final bool isRequired;
   final List<Validation<String>>? validators;
   final void Function(String?)? onSaved;
+  final bool isObscured;
 
   const CustomTextField({
     super.key,
@@ -14,20 +15,39 @@ class CustomTextField extends StatelessWidget {
     this.isRequired = false,
     this.validators,
     this.onSaved,
+    this.isObscured = false,
   });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       autovalidateMode: AutovalidateMode.onUserInteraction,
+      obscureText: widget.isObscured ? _obscureText : false,
       decoration: InputDecoration(
-        labelText: labelText,
+        labelText: widget.labelText,
+        suffixIcon: widget.isObscured
+            ? IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility : Icons.visibility_off,
+                ),
+                onPressed: () => setState(() {
+                  _obscureText = !_obscureText;
+                }),
+              )
+            : null,
       ),
       validator: Validator.apply(context, [
-        if (isRequired) const RequiredValidation(),
-        if (validators != null) ...validators!,
+        if (widget.isRequired) const RequiredValidation(),
+        if (widget.validators != null) ...widget.validators!,
       ]),
-      onSaved: onSaved,
+      onSaved: widget.onSaved,
     );
   }
 }
